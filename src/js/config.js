87463831f7cfae3b5237abc33eb7acf7207c7ffa
hvlvk <vikagevel19@gmail.com>
2025-08-15 17:37:26 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 20;
+export const RES_PER_PAGE = 10;
+export const KEY = '1bd7efab-db7e-4e5e-9528-11003f77e54a';
+export const MODAL_CLOSE_SEC = 2.5;
